c8c2303084ea658ad46a3782657276443885a431
Aidan Priestley <aidanpriestley46@gmail.com>
2026-08-11 17:09:42 +0100

    start to my rps project

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -8,6 +8,8 @@ choices = ["rock", "paper", "scissors"]
 def main():
 
     retry = "yes"
+    score_user = 0
+    score_computer = 0
 
     while retry == "yes":
 
@@ -21,25 +23,37 @@ def main():
 
         program_decision = random.choice(choices)
 
-        logic(user_decision, program_decision)
+        result = logic(user_decision, program_decision)
+
+        if result == "user":
+            score_user = score_user + 1
+        elif result == "computer":
+            score_computer = score_computer + 1
         retry = input("want another go?  ")
+
 
 
 def logic(user_decision, program_decision):
     if user_decision == program_decision:
         print("it was a draw")
     elif user_decision == "rock" and program_decision == "paper":
-        print("computer wins!!")
+        return "computer"
     elif user_decision == "paper" and program_decision == "scissors":
-        print("computer wins")
+        return "computer"
     elif user_decision == "paper" and program_decision == "rock":
-        print("you win!!")
+        return "user"
     elif user_decision == "scissors" and program_decision == "paper":
-        print("you win!!")
+        return "user"
     elif user_decision == "scissors" and program_decision == "rock":
-        print("computer wins!!")
+        return "computer"
     elif user_decision == "rock" and program_decision == "scissors":
-        print("you win!!")
+        return "user"
+
+
+
+
+
+
 
 
 
