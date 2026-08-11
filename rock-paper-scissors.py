390e08b11708bc70b0512ba693172353cefbe234
Aidan Priestley <aidanpriestley46@gmail.com>
2026-08-11 17:53:26 +0100

    added menu with 2 options (show score & quit)

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -7,34 +7,47 @@ choices = ["rock", "paper", "scissors"]
 
 def main():
 
-    retry = "yes"
-    score_user = 0
-    score_computer = 0
 
-    while retry == "yes":
+    while True:
+        print("Rock Paper Scissors Game")
+        print("1. Play Game (1) ")
+        print("2. Show score (2)")
+        print("3. Quit Game (3)")
+        menu_option = input("choose an option: ")
 
-
-
-        user_decision = input("Choose rock, paper or scissors: ").lower()
-        if user_decision not in choices:
-            print("invalid option!!")
-            return
+        if menu_option == "3":
+            break
 
 
-        program_decision = random.choice(choices)
 
-        result = logic(user_decision, program_decision)
+        retry = "yes"
+        score_user = 0
+        score_computer = 0
 
-        if result == "user":
-            score_user = score_user + 1
-            print("you won!!")
-        elif result == "computer":
-            score_computer = score_computer + 1
-            print("the computer won, better luck next time!!")
-        elif result == "draw":
-            print("it was a draw!!")
+        while retry == "yes":
 
-        retry = input("want another go?  ")
+
+
+            user_decision = input("Choose rock, paper or scissors: ").lower()
+            if user_decision not in choices:
+                print("invalid option!!")
+                return
+
+
+            program_decision = random.choice(choices)
+
+            result = logic(user_decision, program_decision)
+
+            if result == "user":
+                score_user = score_user + 1
+                print("you won!!")
+            elif result == "computer":
+                score_computer = score_computer + 1
+                print("the computer won, better luck next time!!")
+            elif result == "draw":
+                print("it was a draw!!")
+
+            retry = input("want another go?  ")
 
 
 
